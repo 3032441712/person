a026de22c144630e5129085c48622f9500a63ecc
zhaoyan <3032441712@qq.com>
2014-09-24 09:27:51 +0800

    本机开发使用的配置文件 不进行上传到github

--- config.php
+++ config.php
@@ -13,9 +13,9 @@
  */
 define('DBHOST', '127.0.0.1');
 define('DBPORT', 3306);
-define('DBUSER', '');
-define('DBPASS', '');
+define('DBUSER', 'person');
+define('DBPASS', '123123');
 define('DBNAME', 'person');
 
 //系统安全密匙
-define('APP_SYSTEM_SECRET_KEY', '');
+define('APP_SYSTEM_SECRET_KEY', '4aa2728a02e1f5964afe44a0f382191c');
